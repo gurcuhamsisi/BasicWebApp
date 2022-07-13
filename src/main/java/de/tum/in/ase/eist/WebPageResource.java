253d49af0c33b82f8207624bb4c938b3339d87c1
BerkeSaylan <berksay@yahoo.com>
2022-07-13 17:11:44 +0200

    "empty commit"

--- src/main/java/de/tum/in/ase/eist/WebPageResource.java
+++ src/main/java/de/tum/in/ase/eist/WebPageResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebPageResource {
 
+
 	private final QueryProcessor queryProcessor;
 
 	public WebPageResource(QueryProcessor queryProcessor) {
